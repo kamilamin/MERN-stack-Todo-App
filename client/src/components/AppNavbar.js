b0198af21f5d34ee3e7abf6353e981748590cdd6
kamilamin <kamil.amin@live.co.uk>
2019-03-09 19:10:05 +0500

    Adding new packages in client

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+
+class AppNavbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+
+export default AppNavbar;
