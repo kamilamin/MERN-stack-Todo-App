f6a1f6ff8fac292bad2a5c4cc17492488557b378
kamilamin <kamil.amin@live.co.uk>
2019-03-09 19:41:21 +0500

    Develop navbar in Client side

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,28 +1,43 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Container
-} from 'reactstrap';
-
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from "reactstrap";
 
 class AppNavbar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    render() {
-        return (
-            <div></div>
-        );
-    }
+  state = {
+    isOpen: false
+  };
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">MERN Todo App</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink href="https://www.github.com/kamilamin">Github</NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
 }
-
 
 export default AppNavbar;
