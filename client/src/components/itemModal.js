12cb2b42a2e5fed61f4aa6cb7b44cc98ae3bce42
kamilamin <kamil.amin@live.co.uk>
2019-04-16 00:18:52 +0500

    Adding data to DB with redux implemented

--- client/src/components/itemModal.js
+++ client/src/components/itemModal.js
@@ -11,7 +11,6 @@ import {
 } from "reactstrap";
 import { connect } from "react-redux";
 import { addItem } from "../actions/itemActions";
-import uuid from 'uuid';
 
 class ItemModal extends Component {
   state = {
@@ -32,7 +31,6 @@ class ItemModal extends Component {
   onSubmit = (ev) => {
     ev.preventDefault();
     const newItem = {
-      id: uuid(),
       name: this.state.name
     }
 
