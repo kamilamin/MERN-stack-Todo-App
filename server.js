237de24b78ee8b30735eec7641ba6f064c109214
kamilamin <kamil.amin@live.co.uk>
2019-07-06 22:31:12 +0500

    --Commit: Updatec code server.js and package.json for deployment

--- server.js
+++ server.js
@@ -18,6 +18,17 @@ mongoose
 // Use routes
 app.use('/api/items', items);
 
+//Serve static assets if we're in production
+if(process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get("*", (req, res) => {
+        res.sendFile(__dirname, 'client', 'build', 'index.html')
+    });
+}
+
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
