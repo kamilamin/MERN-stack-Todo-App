3e272b7fafa3b41c21a48b21878d8658a0a99a98
kamil amin <kamil.amin@live.co.uk>
2019-07-18 11:25:06 -0700

    --Commit: Update the connectionjs file on server side

--- config/Connection.js
+++ config/Connection.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://FirstAuth:babaskt0343@cluster0-xhal9.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://kamilamin:babaskt0343@cluster0-r8wod.mongodb.net/test?retryWrites=true&w=majority'
 }
