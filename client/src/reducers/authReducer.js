b55e91121968fafb0c1fbde2d3ee5418b4245ce5
kamil amin <kamil.amin@live.co.uk>
2019-07-18 11:54:28 -0700

    --Commit: Adding the Auth Reducer code in Redux

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,56 @@
+import {
+  USER_LOADED,
+  USER_LOADING,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  AUTH_ERROR,
+  LOGOUT_SUCCESS,
+} from '../actions/types';
+import {stat} from 'fs';
+
+const initialState = {
+  token: localStorage.getItem ('token'),
+  isAuthenticated: null,
+  isLoading: null,
+  user: null,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case USER_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        isLoading: false,
+        user: action.payload,
+      };
+    case LOGIN_SUCCESS:
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        isLoading: false,
+      };
+    case AUTH_ERROR:
+    case LOGOUT_SUCCESS:
+    case LOGIN_FAIL:
+    case REGISTER_FAIL:
+      return {
+        ...state,
+        token: null,
+        user: null,
+        isAuthenticated: false,
+        isLoading: false,
+      };
+    default:
+      return state;
+  }
+}
