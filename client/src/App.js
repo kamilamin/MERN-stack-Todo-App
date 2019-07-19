90fafae465e0918308b044efbeadb2b3fa644800
kamil amin <kamil.amin@live.co.uk>
2019-07-19 11:55:57 -0700

    --Commit: Connect Action with Redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
-import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import AppNavbar from "./components/AppNavbar";
-import ItemModal from "./components/itemModal";
-import ShoppingList from "./components/ShoppingList";
-import { Container } from "reactstrap";
-import { Provider } from "react-redux";
-import store from "./store";
-import "./App.css";
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppNavbar from './components/AppNavbar';
+import ItemModal from './components/itemModal';
+import ShoppingList from './components/ShoppingList';
+import {Container} from 'reactstrap';
+import {Provider} from 'react-redux';
+import store from './store';
+import {loadUser} from './actions/authActions';
+import './App.css';
 
 class App extends Component {
-  render() {
+  componentDidMount () {
+    store.dispatch (loadUser ());
+  }
+  render () {
     return (
       <Provider store={store}>
         <div className="App">
