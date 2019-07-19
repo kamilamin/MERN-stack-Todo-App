d541a3aafda602088b0dc8d97fd61412f773749f
kamil amin <kamil.amin@live.co.uk>
2019-07-19 12:00:00 -0700

    --Commit: Formating the API auth code

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,68 +1,67 @@
-const express = require("express");
-const bcrypt = require('bcryptjs');
-const config = require('config');
-const jwt = require('jsonwebtoken');
-const auth = require('../../middleware/auth');
+const express = require ('express');
+const bcrypt = require ('bcryptjs');
+const config = require ('config');
+const jwt = require ('jsonwebtoken');
+const auth = require ('../../middleware/auth');
 
-const router = express.Router();
+const router = express.Router ();
 
 // User Model
-const User = require("../../model/User");
+const User = require ('../../model/User');
 
 // @route POST api/auth
 // @decs Auth user
 // @access Public
 
-router.post("/", (req, res) => {
-  const { email, password } = req.body;
+router.post ('/', (req, res) => {
+  const {email, password} = req.body;
 
   // Simple Validations
-  if(!email || !password){
-      return res.status(400).json({
-          message: "Please enter all fields"
-      });
+  if (!email || !password) {
+    return res.status (400).json ({
+      message: 'Please enter all fields',
+    });
   }
   //
-  User.findOne({ email }).then(user => {
-      if(!user){
-          return res.status(400).json({
-              message: "User does not exist"
-          })
-      }
+  User.findOne ({email}).then (user => {
+    if (!user) {
+      return res.status (400).json ({
+        message: 'User does not exist',
+      });
+    }
 
-      // Validate Password
-      bcrypt.compare(password, user.password).then(isMatch => {
-          if(!isMatch) return res.status(400).json({message: 'Invalid Password'})
+    // Validate Password
+    bcrypt.compare (password, user.password).then (isMatch => {
+      if (!isMatch)
+        return res.status (400).json ({message: 'Invalid Password'});
 
-          jwt.sign(
-              {id: user.id},
-              config.get("jwtSecret"),
-              {expiresIn: 3600},
-              (err, token) => {
-                  if(err) throw err;
-                  res.json({
-                      token,
-                      user: {
-                          id: user.id,
-                          username: user.username,
-                          email: user.email
-                      }
-                  })
-              }
-          )
-      })
-
+      jwt.sign (
+        {id: user.id},
+        config.get ('jwtSecret'),
+        {expiresIn: 3600},
+        (err, token) => {
+          if (err) throw err;
+          res.json ({
+            token,
+            user: {
+              id: user.id,
+              username: user.username,
+              email: user.email,
+            },
+          });
+        }
+      );
+    });
   });
 });
 
 // @route GET api/auth/user
 // @decs GET User Data
 // @access Private
-router.get('/user', auth, (req, res) => {
-    User.findById(req.user.id)
-    .select('-password')
-    .then(user => res.json(user));
+router.get ('/user', auth, (req, res) => {
+  User.findById (req.user.id)
+    .select ('-password')
+    .then (user => res.json (user));
 });
-
 
 module.exports = router;
