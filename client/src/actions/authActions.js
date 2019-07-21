bb83c41aab73b91e1b1943c31bfeb386827cb8ee
kamil amin <kamil.amin@live.co.uk>
2019-07-21 12:56:56 -0700

    --Commit: Adding Register method for register new user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -34,6 +34,35 @@ export const loadUser = () => (dispatch, getState) => {
     });
 };
 
+// Register User
+export const register = ({name, email, password}) => dispatch => {
+  // Headers
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  // Request Body
+  const body = JSON.stringify ({name, email, password});
+
+  axios
+    .post ('/api/users', body, config)
+    .then (res =>
+      dispatch ({
+        type: REGISTER_SUCCESS,
+        payload: res.data,
+      })
+    )
+    .catch (err => {
+      dispatch (
+        returnError (err.response.data, err.response.status, 'REGISTER_FAIL')
+      );
+      dispatch ({
+        type: REGISTER_FAIL,
+      });
+    });
+};
+
 // Setup config/headers and token
 export const tokenConfig = getState => {
   // Get token from local storage
